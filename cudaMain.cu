bf6c121dccc1dc8054fcb155ab36e06a726ebf56
Palash Bansal <PalashBansal96@gmail.com>
2018-04-08 01:03:22 +0530

    Added libraries.

--- cudaMain.cu
+++ cudaMain.cu
@@ -3,11 +3,13 @@
 //
 
 #include "cudaMain.h"
-#include "vecAdd.h"
+
 using namespace std;
 
 int cudaMain(int argc, char **argv) {
-    cout<<"Hello"<<endl;
-    doVecAdd();
+
+
+
+
     return 0;
 }
--- cudaMain.h
+++ cudaMain.h
@@ -2,11 +2,11 @@
 // Created by Palash on 19-02-2018.
 //
 
-#ifndef CUDA_TEST_CLION_CUDAMAIN_H
-#define CUDA_TEST_CLION_CUDAMAIN_H
+#ifndef PathTracer_CUDA_CUDAMAIN_H
+#define PathTracer_CUDA_CUDAMAIN_H
 
 #include <iostream>
 #include <cstdio>
 int cudaMain(int argc, char **argv);
 
-#endif //CUDA_TEST_CLION_CUDAMAIN_H
+#endif //PathTracer_CUDA_CUDAMAIN_H
